a8865f4ab3d2a0ce75ef4bff6ec4ceb4bddf5ab7
victor dias <victor.dias@meumobi.com>
2017-07-06 00:57:42 +0200

    FIX: allow emails with char '+'

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -4,7 +4,7 @@ import { FormControl } from '@angular/forms';
 export class EmailValidator {
 
   static isValid(control: FormControl){
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(control.value);
+    const re = /^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(control.value);
 
     if (re){
       return null;
