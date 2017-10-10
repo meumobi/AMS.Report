87646d9bd3fdc5347b36be1c06f10df07f87e06b
Daniel Antonio Conte <danconte72@github.com>
2017-10-10 10:10:00 -0300

    ENHANCEMENT: Password origin in setLatestUnpluggedImport Cloud Function - The function get the password to check from firebase database on settings.password node;

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,31 +5,33 @@ const moment = require('moment');
 admin.initializeApp(functions.config().firebase);
 
 exports.setLatestUnpluggedImport = functions.https.onRequest((req, res) => {
-  const token = "2e6f9b0d5885b6010f9167787445617f553a735f";
-  const psw = sha1(req.query.psw);
-  if (token == psw){
-    var latestUnpluggedImport;
-    if (req.query.date){
-      latestUnpluggedImport = moment(req.query.date);
-    } else {
-      latestUnpluggedImport = moment(new Date());
-    }
-    if (latestUnpluggedImport.isValid()){
-      latestUnpluggedImport = latestUnpluggedImport.format("YYYY-MM-DD");
-      admin.database().ref('/settings').update({latestUnpluggedImport: latestUnpluggedImport}).then(snapshot => {
-        console.log("Latest Unplugged Import Updated");
-        res.send("ok");
-      }).catch(function(error) {
-        console.log("Error Updating Latest Unplugged Import:", error);
-        res.send("nok");      
-      });
-    } else {
-      console.log("Wrong date format");
-      res.send("nok");
-    }
-    
-  } else {
-    console.log("Wrong psw");
-    res.send("nok");
-  }
+  admin.database().ref('/settings').once('value').then(function(snapshot) {
+    const token = sha1(snapshot.val().password);
+      const psw = sha1(req.query.psw);
+      if (token == psw){
+        var latestUnpluggedImport;
+        if (req.query.date){
+          latestUnpluggedImport = moment(req.query.date);
+        } else {
+          latestUnpluggedImport = moment(new Date());
+        }
+        if (latestUnpluggedImport.isValid()){
+          latestUnpluggedImport = latestUnpluggedImport.format("YYYY-MM-DD");
+          admin.database().ref('/settings').update({latestUnpluggedImport: latestUnpluggedImport}).then(snapshot => {
+            console.log("Latest Unplugged Import Updated");
+            res.send("ok");
+          }).catch(function(error) {
+            console.log("Error Updating Latest Unplugged Import:", error);
+            res.send("nok");      
+          });
+        } else {
+          console.log("Wrong date format");
+          res.send("nok");
+        }
+        
+      } else {
+        console.log("Wrong psw");
+        res.send("nok");
+      }
+  });  
  });
