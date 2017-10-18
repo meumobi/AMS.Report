90bd4c9fdad2439e83bda921aa6dd635fd27f444
victor dias <victor.dias@meumobi.com>
2017-10-18 11:13:32 +0100

    ENHANCE: hide statusBar and splashScreen actions , useless on browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -108,8 +108,8 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      //this.statusBar.styleDefault();
+      //this.splashScreen.hide();
     });
   }
 
