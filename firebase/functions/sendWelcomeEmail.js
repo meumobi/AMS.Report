95f434c4c8c9c0601b2fa4bc61139b8132e0d80a
Daniel Antonio Conte <danconte72@github.com>
2017-10-20 14:05:40 -0200

    ENHANCEMENT: Closes #61, add enabled config field for sendWelcomeEmail Functions Command to Turn On: ```dash $ firebase functions:config:set send_welcome_email.enabled=true ``` To Turno Off ```dash $ firebase functions:config:set send_welcome_email.enabled=false ```

--- firebase/functions/sendWelcomeEmail.js
+++ firebase/functions/sendWelcomeEmail.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
 const gmailEmail = encodeURIComponent(functions.config().gmail.email);
 const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const enabled = functions.config().send_welcome_email.enabled;
 const mailTransport = nodemailer.createTransport(
     `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
@@ -10,15 +11,23 @@ const APP_NAME = 'AD.MYSPORTS';
 /*
   Require environment configuration, run following firebase command to set gmail.email and gmail.password values:
   $ firebase functions:config:set gmail.email="[SENDER EMAIL HERE]" gmail.password="[SENDER MAILBOX PASSWORD HERE]"
+  
+  To turn on run:
+  $ firebase functions:config:set send_welcome_email.enabled=true
+  To turn off:
+  $ firebase functions:config:set send_welcome_email.enabled=false 
+
 */
 exports.handler = event => {
   const user = event.data; 
   const email = user.email; 
   const displayName = user.displayName;
-
-  console.log('New welcome email disabled, to:', email);
-  return null;
-  //return sendWelcomeEmail(email, displayName,user);
+  if (enabled == "true"){
+    return sendWelcomeEmail(email, displayName,user);
+  } else {
+    console.log('New welcome email disabled, to:', email);
+    return null;
+  }
 };
 
 function sendWelcomeEmail(email, displayName,user)   {
