676ed1cc11e7f8cc79b61614b777d9b423eacd2d
Daniel  Conte <danielconte@MacBook-Pro-de-Daniel.local>
2018-01-09 13:25:02 -0200

    FIX: Closes #88, Update Cloud Function to function properly

--- firebase/functions/updateUser.js
+++ firebase/functions/updateUser.js
@@ -5,14 +5,12 @@ exports.handler = event => {
   const emailPrev = event.data.previous.child("email");
   const id = event.params.id;
   const email = userData.child("email");
-  const password = userData.child("password");
-  if (email.changed() || password.changed()){
+  if (email.changed()){
     admin.auth().getUserByEmail(emailPrev.val())
     .then(function(user) {
       const uid = user.uid;
       admin.auth().updateUser(uid,{
         email: `${email.val()}`,
-        password: `${password.val()}`,
       })
       .then(function(userRecord) {
         console.log("Successfully updated user:", userRecord.uid);
