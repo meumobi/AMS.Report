58d9f84722078f209f832895215f57eef1ed08e3
Daniel Antonio Conte <danconte72@gmail.com>
2018-12-12 10:12:37 -0200

    feat: Add site.type property
    closes #100

--- src/models/site.model.ts
+++ src/models/site.model.ts
@@ -6,15 +6,18 @@ export interface ISite {
   editor_id: string;
   createdAt: Object;
   url?: string;
+  type: string;
 }
 
 export class Site implements ISite {
   title;
   editor_id;
+  type;
   createdAt = firebase.database.ServerValue.TIMESTAMP;
 
   constructor(title: string, editorId: string) {
     this.title = title;
     this.editor_id = editorId;
+    this.type = 'MarketPlace';
   }
 }
